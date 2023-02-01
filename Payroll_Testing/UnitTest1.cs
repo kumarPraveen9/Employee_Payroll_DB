7e62983490062ff712e2c520a271e1f6f502258a
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-02-01 21:44:23 +0530

    Add files via upload

---
+++ Payroll_Testing/UnitTest1.cs
@@ -0,0 +1,50 @@
+using System;
+using NUnit.Framework;
+using Employee_Payroll;
+using System.Collections.Generic;
+
+namespace Payroll_Testing
+{
+    public class Tests
+    {
+        Program program;
+        Employee employee;
+        [SetUp]
+        public void Setup()
+        {
+            program = new Program();
+            employee = new Employee();
+        }
+
+        [Test]
+        public void CheckFor_Connection_Establishment()
+        {
+            string result = program.EstablishConnection();
+            Assert.AreEqual("Connection was established.",result);
+        }
+        [Test]
+        public void Retrieve_Details_FromDatabase()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees = program.RetrieveFromDatabase();
+            Assert.IsNotNull(employees);
+        }
+        [Test]
+        public void Update_Salary_ForTushar()
+        {
+            int Emp_ID = 1;
+            Int64 newSalary = 300000;
+            employee = program.UpdateSalary(Emp_ID, newSalary);
+            Assert.AreEqual(newSalary, employee.Salary);
+        }
+        [Test]
+        public void Retrieve_Details_FromDatabase_BetweenGivenDates()
+        {
+            List<Employee> employees = new List<Employee>();
+            DateTime FirstDate = DateTime.Parse("2017-01-01");
+            DateTime LastDate = DateTime.Parse("2018-12-31");
+            employees = program.RetrieveFromDatabase_BetweenGivenDates(FirstDate, LastDate);
+            Assert.AreEqual(3, employees.Count);
+        }
+    }
+}
